367e36664c311263dce263e00d652f98304f4073
MuhammadSirajAbbasi <64827860+Siraj454@users.noreply.github.com>
2020-06-17 20:51:03 +0500

    Update child.js

--- src/child.js
+++ src/child.js
@@ -6,7 +6,7 @@ function Child(){
     
     return (
         <dev>
-               <h2>it is First Child its value is {counterValue}</h2>
+               <h3>it is First Child its value is {counterValue}</h3>
                <br></br>
             <button onClick={()=>{counterValue[1](++counterValue[0])}}>
                 Increment</button>
